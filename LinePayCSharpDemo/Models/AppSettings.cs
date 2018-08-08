850d0a2cac525cc9af31c742bd1629f6d4e8a624
Kenichiro Nakamura <kenakamu@microsoft.com>
2018-08-08 13:12:49 +0900

    before

--- LinePayCSharpDemo/Models/AppSettings.cs
+++ LinePayCSharpDemo/Models/AppSettings.cs
@@ -1,20 +1,6 @@
 namespace LinePayCSharpDemo.Models
 {
     public class AppSettings
-    {
-      public LinePay LinePay { get; set; }
-      public LineBot LineBot { get; set; }
-      public string ServerUri{ get; set; }
+    {      
     }
-    public class LinePay
-    {
-        public string ChannelId { get; set; }
-        public string ChannelSecret { get; set; }
-        public bool IsSandbox { get; set; }
-    }    
-    public class LineBot
-    {
-        public string ChannelSecret { get; set; }
-        public string ChannelAccessToken { get; set; }
-    }    
 }
